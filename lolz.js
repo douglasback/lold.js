d787220226c0dec4fde79f30a0812e7aee447113
Douglas Back <douglas@bluestatedigital.com>
2011-11-28 10:05:58 -0500

    added chongchong

--- lolz.js
+++ lolz.js
@@ -2,6 +2,7 @@ var clips = {
     "thingsucks" : './sfx/fts.mp3',
     "forensic" : './sfx/forensic.mp3',
     "facebook" : './sfx/facebook.mp3'
+    "chongchong" : './sfx/chongchong.mp3'
 };
 
 exports.clips = clips;
