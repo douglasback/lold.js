0b480957dc5bcc166b7375c48220a93a51c51018
Douglas Back <douglas@bluestatedigital.com>
2011-11-30 10:13:44 -0500

    Edited usage instructions for script to include 'list'

--- lol.sh
+++ lol.sh
@@ -4,7 +4,9 @@ SERVER='http://itunes.local'
 PORT='8001'
 if [ ! -n "$1" ]
 then
+    echo ""
     echo "lol: play a hilarious sound effect on the stereo"
+    echo "Usage: lol list - fetch a list of installed sound effects on lold.js"
     echo "Usage: lol (sfx)"
     exit
 fi
